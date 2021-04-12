cbd6e1c1e5ae69d19d065db4718f50cb1c07439e
Jeongmin <tdcian71@gmail.com>
2021-04-12 09:20:04 +0900

    깊이 우선 탐색 학습

--- python_basic/4.12 ~ 4.18/algorith_depth_first_search.py
+++ python_basic/4.12 ~ 4.18/algorith_depth_first_search.py
@@ -1,26 +1,25 @@
 """
-너비 우선 탐색 (Breadth-First Search)
+깊이 우선 탐색(Depth-First Search)
+
 1. BFS와 DFS란?
 - 대표적인 그래프 탐색 알고리즘
-    - 너비 우선 탐색(Breath First Search): 정점들과 같은 레벨에 있는 노드들(형제 노드들)을 먼저 탐색하는 방식
+    - 너비 우선 탐색(Breadth First Search): 정점들과 같은 레벨에 있는 노드들(형제 노드들)을 먼저 탐색하는 방식
     - 깊이 우선 탐색(Depth First Search): 정점의 자식들을 먼저 탐색하는 방식
 
-- BFS/DFS 방식 이해를 위한 예제
+- BFS/ DBF 방식 이해를 위한 예제
     - BFS 방식: A - B - C - D - G - H - I - E - F - J
         - 한 단계씩 내려가면서, 해당 노드와 같은 레벨에 있는 노드들(형제 노드들)을 먼저 순회함
     - DFS 방식: A - B - D - E - F - C - G - H - I - J
         - 한 노드의 자식을 타고 끝까지 순회한 후, 다시 돌아와서 다른 형제들의 자식을 타고 내려가며 순회함
 
-2. 파이썬으로 그래프를 표현하는 방법
-- 파이썬에서 제공하는 딕셔너리와 리스트 자료 구조를 활용해서 그래프를 표현할 수 있음
 
-3. BFS 알고리즘 구현
-- 자료구조 큐를 활용함
-    - need_visit 큐와 visited 큐, 두 개의 큐를 생성
-    - 큐의 구현은 간단히 파이썬 리스트를 활용
+3. DFS 알고리즘 구현
+- 자료구조 스택과 큐를 활용함
+    - need_visit 스택과 visted 큐, 두 개의 자료구조를 생성
+    - BFS 자료구조는 두 개의 큐를 활용하는데 반해, DFS는 스택과 큐를 활용한다는 차이가 있음을 인지해야 함
 
 4. 시간 복잡도
-- 일반적인 BFS 시간 복잡도
+- 일반적인 DFS 시간 복잡도
     - 노드 수: V
     - 간선 수: E
         - 위 코드에서 while need_visit은 V + E 번 만큼 수행함
@@ -40,25 +39,17 @@ graph['H'] = ['C']
 graph['I'] = ['C', 'J']
 graph['J'] = ['I']
 
-print(graph)
-
-# 너비 우선 탐색 코드
-def bfs(graph, start_node):
-    visited = list()
-    need_visit = list()
-
+# DFS 그래프 코드로 구현하기
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
     need_visit.append(start_node)
 
     while need_visit:
-        node = need_visit.pop(0)
+        node = need_visit.pop()
         if node not in visited:
             visited.append(node)
             need_visit.extend(graph[node])
 
     return visited
 
-print("너비우선 탐색: ", bfs(graph, 'A')) 
-# -> 너비우선 탐색:  ['A', 'B', 'C', 'D', 'G', 'H', 'I', 'E', 'F', 'J']
-
-
-
+print("깊이 우선 탐색: ",dfs(graph, 'A'))
